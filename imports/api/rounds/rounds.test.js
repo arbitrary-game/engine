4ad8b44c9b4162752821b941ded0673bda0691bc
Ivan Rozhkov <ivan133133@gmail.com>
2017-01-19 11:46:03 +0300

    additional tests + tests fix

--- imports/api/rounds/rounds.test.js
+++ imports/api/rounds/rounds.test.js
@@ -1,5 +1,6 @@
 'use strict';
 /* eslint-env mocha */
+import _ from 'lodash';
 
 import { Meteor } from 'meteor/meteor';
 import { expect } from 'meteor/practicalmeteor:chai';
@@ -7,35 +8,6 @@ import { expect } from 'meteor/practicalmeteor:chai';
 import Round from './rounds.js';
 
 if (Meteor.isServer) {
-  // describe('Rounds calulate', () => {
-  //     const round = new Round([
-  //       {
-  //         player: 'a', stash: 500, bet: 100, stake: 300, vote: 'a'
-  //       },
-  //       {
-  //         player: 'b', stash: 500, bet: 100, stake: 100, vote: 'b'
-  //       },
-  //       {
-  //         player: 'c', stash: 500, bet: 0, stake: 400, vote: 'b'
-  //       },
-  //       {
-  //         player: 'd', stash: 500, bet: 0, stake: 500, vote: 'b'
-  //       },
-  //       {
-  //         player: 'e', stash: 500, bet: 0, stake: 10, vote: 'b'
-  //       }
-  //     ]);
-  //
-  //     it('Report calculate', () => {
-  //       const result = round.calculate();
-  //       expect(result.length).to.equal(5);
-  //       for (const row of result) {
-  //         expect(row).to.have.property('power');
-  //       }
-  //
-  //     });
-  //   });
-
   describe('Rounds calculatePower', () => {
       const round = new Round([
         {
@@ -58,7 +30,7 @@ if (Meteor.isServer) {
       it('Report calculatePower', () => {
         round.calculatePower();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('power');
         }
@@ -88,13 +60,13 @@ if (Meteor.isServer) {
         round.calculatePower();
         round.calculateWinner();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('winner');
         }
-        expect(result[0].winner).to.equal(false);
-        expect(result[1].winner).to.equal(true);
-        expect(result[2].winner).to.equal(null);
+        expect(result[0].winner).to.be.equal(false);
+        expect(result[1].winner).to.be.equal(true);
+        expect(result[2].winner).to.be.equal(null);
       });
     });
 
@@ -122,13 +94,13 @@ if (Meteor.isServer) {
         round.calculateWinner();
         round.calculateMajority();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('majority');
         }
-        expect(result[0].majority).to.equal(false);
-        expect(result[1].majority).to.equal(true);
-        expect(result[2].majority).to.equal(true);
+        expect(result[0].majority).to.be.equal(false);
+        expect(result[1].majority).to.be.equal(true);
+        expect(result[2].majority).to.be.equal(true);
       });
     });
 
@@ -157,15 +129,15 @@ if (Meteor.isServer) {
         round.calculateMajority();
         round.calculateShare();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('share');
         }
-        expect(result[0].share).to.equal(-1);
-        expect(result[1].share).to.equal(0.1);
-        expect(result[2].share).to.equal(0.4);
-        expect(result[3].share).to.equal(0.5);
-        expect(result[4].share).to.equal(0.01);
+        expect(result[0].share).to.be.equal(-1);
+        expect(result[1].share).to.be.equal(0.1);
+        expect(result[2].share).to.be.equal(0.4);
+        expect(result[3].share).to.be.equal(0.5);
+        expect(result[4].share).to.be.equal(0.01);
       });
     });
 
@@ -195,15 +167,15 @@ if (Meteor.isServer) {
         round.calculateShare();
         round.calculateScalp();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('scalp');
         }
-        expect(result[0].scalp).to.equal(-300);
-        expect(result[1].scalp).to.equal(30);
-        expect(result[2].scalp).to.equal(120);
-        expect(result[3].scalp).to.equal(150);
-        expect(result[4].scalp).to.equal(3);
+        expect(result[0].scalp).to.be.equal(-300);
+        expect(result[1].scalp).to.be.equal(30);
+        expect(result[2].scalp).to.be.equal(120);
+        expect(result[3].scalp).to.be.equal(150);
+        expect(result[4].scalp).to.be.equal(3);
       });
     });
 
@@ -234,15 +206,15 @@ if (Meteor.isServer) {
         round.calculateScalp();
         round.calculatePrize();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('prize');
         }
-        expect(result[0].prize).to.equal(-100);
-        expect(result[1].prize).to.equal(100);
-        expect(result[2].prize).to.equal(0);
-        expect(result[3].prize).to.equal(0);
-        expect(result[4].prize).to.equal(0);
+        expect(result[0].prize).to.be.equal(-100);
+        expect(result[1].prize).to.be.equal(100);
+        expect(result[2].prize).to.be.equal(0);
+        expect(result[3].prize).to.be.equal(0);
+        expect(result[4].prize).to.be.equal(0);
       });
     });
 
@@ -274,15 +246,15 @@ if (Meteor.isServer) {
         round.calculatePrize();
         round.calculateFix();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('fix');
         }
-        expect(result[0].fix).to.equal(-3);
-        expect(result[1].fix).to.equal(0);
-        expect(result[2].fix).to.equal(0);
-        expect(result[3].fix).to.equal(0);
-        expect(result[4].fix).to.equal(0);
+        expect(result[0].fix).to.be.equal(-3);
+        expect(result[1].fix).to.be.equal(0);
+        expect(result[2].fix).to.be.equal(0);
+        expect(result[3].fix).to.be.equal(0);
+        expect(result[4].fix).to.be.equal(0);
       });
     });
 
@@ -315,15 +287,57 @@ if (Meteor.isServer) {
         round.calculateFix();
         round.calculateTotal();
         const result = round.roundParams;
-        expect(result.length).to.equal(5);
+        expect(result.length).to.be.equal(5);
         for (const row of result) {
           expect(row).to.have.property('total');
         }
-        expect(result[0].total).to.equal(97);
-        expect(result[1].total).to.equal(630);
-        expect(result[2].total).to.equal(620);
-        expect(result[3].total).to.equal(650);
-        expect(result[4].total).to.equal(503);
+        expect(result[0].total).to.be.equal(97);
+        expect(result[1].total).to.be.equal(630);
+        expect(result[2].total).to.be.equal(620);
+        expect(result[3].total).to.be.equal(650);
+        expect(result[4].total).to.be.equal(503);
       });
     });
+
+  describe('Rounds calulate', () => {
+      const round = new Round([
+        {
+          player: 'a', stash: 500, bet: 100, stake: 300, vote: 'a'
+        },
+        {
+          player: 'b', stash: 500, bet: 100, stake: 100, vote: 'b'
+        },
+        {
+          player: 'c', stash: 500, bet: 0, stake: 400, vote: 'b'
+        },
+        {
+          player: 'd', stash: 500, bet: 0, stake: 500, vote: 'b'
+        },
+        {
+          player: 'e', stash: 500, bet: 0, stake: 10, vote: 'b'
+        }
+      ]);
+
+      it('Report calculate', () => {
+        const result = round.calculate();
+        expect(result.length).to.be.equal(5);
+        for (const row of result) {
+          expect(row).to.have.property('power');
+          expect(row).to.have.property('winner');
+          expect(row).to.have.property('majority');
+          expect(row).to.have.property('share');
+          expect(row).to.have.property('scalp');
+          expect(row).to.have.property('prize');
+          expect(row).to.have.property('fix');
+          expect(row).to.have.property('total');
+
+          expect(row.bet + row.stake).to.be.at.most(row.stash);
+        }
+        const totalRound =  _.sumBy(result, i => i.total);
+        const previosTotal =  _.sumBy(result, i => i.stash);
+        expect(totalRound).to.be.equal(previosTotal);
+
+      });
+    });
+
 }
