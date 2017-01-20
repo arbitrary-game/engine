cc6a8cb6f7991e87547fb8c6feeb5dddca5f854b
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2017-01-20 04:30:26 +0300

    Add React Accounts-UI.

--- imports/ui/AppComponent.js
+++ imports/ui/AppComponent.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from 'react'
 import {Container} from 'semantic-ui-react'
 
 import TopMenu from './TopMenu'
 
-export default class extends React.Component {
+import Tracker from 'tracker-component'
+import Redirect from 'react-router/Redirect'
+
+export default class extends Tracker.Component {
+  constructor(props) {
+    super(props);
+
+    this.autorun(() => {
+      this.setState({
+        isAuthenticated: Meteor.user()
+      });
+    });
+  }
+
   render() {
+    if ( ! this.state.isAuthenticated) return <Redirect to="signin"/>;
+
     return (
       <div>
         <TopMenu />
--- imports/ui/Router.js
+++ imports/ui/Router.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {BrowserRouter, Match, Redirect} from 'react-router'
+import {Container} from 'semantic-ui-react'
 
 import AppContainer from './AppContainer'
 import GamesListContainer from './Games/GamesListContainer'
 import GamesCreateContainer from './Games/GamesCreateContainer'
 
+import { Accounts, STATES } from 'meteor/std:accounts-ui'
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL_NO_PASSWORD',
+  profilePath: "/"
+});
+
 export const renderRoutes = () => (
   <BrowserRouter>
-    <AppContainer>
-      <Match exactly pattern="/" render={routerProps => <Redirect to="/games"/>} />
-      <Match exactly pattern="/games" component={GamesListContainer} />
-      <Match exactly pattern="/games/create" component={GamesCreateContainer} />
-    </AppContainer>
+    <Container>
+      <Match exactly pattern="/signin" component={() => <Accounts.ui.LoginForm />} />
+      <Match exactly pattern="/signup" component={() => <Accounts.ui.LoginForm formState={STATES.SIGN_UP} />} />
+      <AppContainer>
+        <Match exactly pattern="/" render={routerProps => <Redirect to="/games"/>} />
+        <Match exactly pattern="/games" component={GamesListContainer} />
+        <Match exactly pattern="/games/create" component={GamesCreateContainer} />
+      </AppContainer>
+    </Container>
   </BrowserRouter>
 );
