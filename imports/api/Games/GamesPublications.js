e8d5c2df1d9de9807743da6fb7c6c0bddaa005e7
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2017-01-22 22:17:06 +0300

    Unstub active game list.

--- imports/api/Games/GamesPublications.js
+++ imports/api/Games/GamesPublications.js
@@ -1,26 +1,40 @@
 import {Meteor} from 'meteor/meteor'
 import Games from './GamesCollection'
+import Players from '../Players/PlayersCollection'
+import Users from '../Users/UsersCollection'
 
-Meteor.publish('Games.active', function () {
+Meteor.publishComposite('Games.active', function () {
   if (!this.userId) return this.ready();
-  return Games.find({isStarted: false, isPublic: true}, Games.publicFields);
+  return {
+    find: () => Games.find({isStarted: false, isPublic: true}, Games.publicFields),
+    children: [
+      {
+        find: game => Players.find({gameId: game._id}, {fields: Players.publicFields}),
+        children: [
+          {
+            find: player => Users.find({_id: player.userId}, {fields: Users.publicFields})
+          }
+        ]
+      }
+    ]
+  };
 });
 
-// Meteor.publishComposite('Games.showById', function(_id) {
-//   check(_id, String);
-//   if (!this.userId) return this.ready();
-//
-//   return {
-//     find: () => Games.find({_id}),
-//     children: [
-//       {
-//         find: game => Players.find({gameId: game._id}, {fields: Players.publicFields})
-//         children: [
-//           {
-//             find: player => Users.find({_id: player.userId}, {fields: Users.publicFields})
-//           }
-//         ]
-//       }
-//     ]
-//   };
-// });
+Meteor.publishComposite('Games.showById', function(_id) {
+  check(_id, String);
+  if (!this.userId) return this.ready();
+
+  return {
+    find: () => Games.find({_id}),
+    children: [
+      {
+        find: game => Players.find({gameId: game._id}, {fields: Players.publicFields}),
+        children: [
+          {
+            find: player => Users.find({_id: player.userId}, {fields: Users.publicFields})
+          }
+        ]
+      }
+    ]
+  };
+});
