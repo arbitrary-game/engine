5132e76498f7187b82354f7b3658851f903a8242
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2017-01-22 14:01:24 +0300

    Translate "reset password" form. Improve translation.

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -3,13 +3,18 @@ import { Meteor } from 'meteor/meteor'
 import { SSR } from 'meteor/meteorhacks:ssr'
 
 SSR.compileTemplate('verificationEmailTemplate', Assets.getText('verificationEmailTemplate.html'));
+SSR.compileTemplate('resetPasswordTemplate', Assets.getText('resetPasswordTemplate.html'));
 SSR.compileTemplate('verificationEmailTemplateText', Assets.getText('verificationEmailTemplateText.html'));
+SSR.compileTemplate('resetPasswordTemplateText', Assets.getText('resetPasswordTemplateText.html'));
 
 Accounts.emailTemplates.from = Meteor.settings.public.emails.from;
 Accounts.emailTemplates.siteName = Meteor.settings.public.siteName;
 Accounts.emailTemplates.verifyEmail.subject = (user) => Meteor.settings.public.emails.verification.subject;
 Accounts.emailTemplates.verifyEmail.text = (user, url) => SSR.render("verificationEmailTemplateText", {user, url});
 Accounts.emailTemplates.verifyEmail.html = (user, url) => SSR.render("verificationEmailTemplate", {user, url});
+Accounts.emailTemplates.resetPassword.subject = (user) => Meteor.settings.public.emails.reset.subject;
+Accounts.emailTemplates.resetPassword.text = (user, url) => SSR.render("resetPasswordTemplateText", {user, url});
+Accounts.emailTemplates.resetPassword.html = (user, url) => SSR.render("resetPasswordTemplate", {user, url});
 
 Accounts.onCreateUser((options, user) => {
 
