54e3a4149cd530e9a365d21b4c73feadc0f8a294
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2017-01-22 22:43:06 +0300

    Add Players fixtures.

---
+++ imports/api/Players/PlayersFixturesData.js
@@ -0,0 +1,18 @@
+export default {
+  Yandex2GamePlayer: {
+    gameId: "Yandex2Game",
+    userId: "AliceRipleyUser"
+  },
+  TopSecretGamePlayer: {
+    gameId: "TopSecretGame",
+    userId: "BobDylanUser"
+  },
+  ProSeriesGamePlayer: {
+    gameId: "ProSeriesGame",
+    userId: "WinstonChirchillUser"
+  },
+  ProSeriesGamePlayer2: {
+    gameId: "ProSeriesGame",
+    userId: "BobDylanUser"
+  }
+};
