9b67e926e1ebdf67170c1b65b19d27793ea730d1
Ivan Rozhkov <ivan133@MacBook-Air-Ivan.local>
2017-01-23 12:54:37 +0300

    development fix

--- imports/common/IDValidator.js
+++ imports/common/IDValidator.js
@@ -1,6 +1,9 @@
 import {SimpleSchema} from 'meteor/aldeed:simple-schema';
 
 export default function() {
+  if (Meteor.settings.public.isDebug){
+    return true
+  }
   if (this.value.match(SimpleSchema.RegEx.Id)) {
     return true;
   } else if (this.userId === null && this.isInsert && this.docId && !this.docId.match(SimpleSchema.RegEx.Id)) {
