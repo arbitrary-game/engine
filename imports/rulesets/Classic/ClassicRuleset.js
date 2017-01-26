cbbc45f5f65e0eea973cc8d9fab289ca520930c7
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2017-01-27 02:54:33 +0300

    Improve ClassicRuleset. Provide tests.

--- imports/rulesets/Classic/ClassicRuleset.js
+++ imports/rulesets/Classic/ClassicRuleset.js
@@ -1,103 +1,147 @@
-import {last, find, without} from "lodash";
+import {values, indexOf, pick, each, map, every, findLastIndex, first, last, find, findLast, without, remove, sortBy} from "lodash";
 import ClassicRound from './ClassicRound';
 
 export default class ClassicRuleset {
-  constructor(actions, players, initiator) {
-    this.actions = actions;
+  constructor(actions, players) {
+    this.actions = sortBy(actions, "createdAt");
     this.players = players;
-    this.initiator = initiator;
   }
 
   getState() {
     let pendingActions = [];
-    let rounds = [];
-    let round = this.createRound();
+    let messages = [];
 
-    let previousAction;
-    console.log('this.actions', this.actions);
-    if (!this.actions.length){
-      pendingActions.push({
-        //TODO we should get initiator here
-        playerId: this.initiator,
-        type: "ChooseOpponent",
-      });
-    }
-    for (let action of this.actions) {
-      console.log('action', action);
-      console.log('previousAction', previousAction);
+    each(this.actions, (action, index, actions) => {
+      const processedActions = actions.slice(0, index + 1);
+      const roundActions = this.getRoundActions(processedActions);
+
+      messages.push(action);
 
       switch (action.type) {
         case "ChooseOpponent":
-          pendingActions = [];
-          pendingActions.push({
-            playerId: action.playerId,
-            type: "Raise",
-            amount: this.getMinimalBetAmount(),
-          });
+          const result = this.createRaiseOrCallAction(action["playerId"], this.getMinimalBetAmount());
+          pendingActions.push(result);
           break;
         case "Raise":
-          if (previousAction.type === 'ChooseOpponent') {
-            pendingActions = [];
-            pendingActions.push({
-              playerId: previousAction.opponentId,
-              type: "Raise",
-              amount: action.amount,
-            });
-          }
-           else if ((previousAction && previousAction.amount && action.amount > previousAction.amount) || (!previousAction && action.amount)) {
-            // TODO looks like these fields are not in round now round
-            const opponent = find(round, (info) => info.bet && info.playerId != action.playerId);
-            pendingActions = [];
-            pendingActions.push({
-              playerId: opponent.playerId,
-              type: "Raise",
-              amount: action.amount,
-            });
+          const previousBet = this.findPreviousBetFor(roundActions);
+          const bet = action["amount"];
+          const opponentId = this.findOpponentIdFor(action.playerId, roundActions);
+
+          if (!previousBet || bet > previousBet) {
+            const result = this.createRaiseOrCallAction(opponentId, bet);
+            pendingActions = [result];
           } else {
-            let playerA = find(round, {playerId: action.playerId});
-            let playerB = find(round, {playerId: previousAction.playerId});
-            playerA.bet = playerB.bet = action.amount;
-            pendingActions = [];
-            for (const player of this.players) {
-              pendingActions.push({
-                type: "Stake",
-                amount: this.getMinimalStakeAmount(),
-                playerId: player._id
-              });
-            }
+            pendingActions = map(this.players, player => this.createStakeActionFor(player));
           }
           break;
         case "Stake":
-          pendingActions = without(pendingActions, (pendingAction) => pendingAction.playerId === action.playerId);
+          remove(pendingActions, pendingAction => pendingAction.playerId == action.playerId);
+
+          // staking finished
+          if (!pendingActions.length) {
+            pendingActions = map(this.players, player => this.createVoteActionFor(player));
+          }
           break;
         case "Vote":
-          // if everybody voted, calculate round results & push the new round
+          remove(pendingActions, pendingAction => pendingAction.playerId == action.playerId);
+
+          // voting finished
+          if (!pendingActions.length) {
+            messages.push(this.calculateResult(roundActions));
+          }
           break;
         default:
           throw new Error(`Undefined action type: ${action.type}`);
-          break;
       }
-      previousAction = action;
+    });
+
+    // a round just started
+    if (!pendingActions.length) {
+      pendingActions.push(this.createChooseOpponentAction());
     }
 
-    // TODO: copy stashes into next round
-
-    rounds.push(round);
-    return {pendingActions, rounds};
+    return {pendingActions, messages};
   };
 
-  createRound() {
-    let round = new ClassicRound(this, []);
-    for (let player of this.players) {
-      round.data.push({
-        playerId: player._id,
-        stash: 0,
-        bet: 0,
-        stake: 0,
-        votedForPlayerId: "",
-      });
+  calculateResult(actions) {
+    // TODO calculate data for Round here
+    const data = map(this.players, player => ({
+      playerId: player._id,
+      stash: player.stash,
+      bet: this.getPlayerBetFor(actions, player._id),
+      stake: this.getPlayerStakeFor(actions, player._id),
+      votedForPlayerId: this.getCandidateIdFor(actions, player._id)
+    }));
+
+    const round = new ClassicRound(this, data);
+    const result = round.calculate();
+    return result;
+  }
+
+  getCandidateIdFor(actions, playerId) {
+    return find(actions, action => action.type == "Vote" && action.playerId == playerId).candidateId;
+  }
+
+  getPlayerStakeFor(actions, playerId) {
+    return find(actions, action => action.type == "Stake" && action.playerId == playerId).amount;
+  }
+
+  getPlayerBetFor(actions, playerId) {
+    const opponents = values(pick(find(actions, {type: "ChooseOpponent"}), "playerId", "opponentId"));
+    return indexOf(opponents, playerId) != -1 ? findLast(actions, action => action.type == "Raise").amount : 0;
+  }
+
+  findOpponentIdFor(playerId, actions) {
+    const action = find(actions, {type: "ChooseOpponent"});
+    return action.playerId == playerId ?  action.opponentId : action.playerId;
+  }
+
+  findPreviousBetFor(actions) {
+    const beforeLast = actions.length - 2;
+
+    const previous = this.actions[beforeLast];
+    if (previous.type == "Raise") {
+      return previous.amount;
     }
-    return round;
+  }
+
+  getRoundActions(actions) {
+    const from = findLastIndex(actions, {type: "ChooseOpponent"});
+    return actions.slice(from);
+  }
+
+  createVoteActionFor(player) {
+    return {
+      type: "Vote",
+      playerId: player._id
+    }
+  }
+
+  createStakeActionFor(player) {
+    return {
+      type: "Stake",
+      amount: this.getMinimalStakeAmount(),
+      playerId: player._id
+    }
+  }
+
+  createChooseOpponentAction() {
+    return {
+      type: "ChooseOpponent",
+      playerId: this.findInitiator()._id
+    }
+  }
+
+  createRaiseOrCallAction(playerId, amount) {
+    return {
+      type: "Raise",
+      playerId,
+      amount
+    }
+  }
+
+  findInitiator() {
+    return first(sortBy(this.players, ["stash", "createdAt"]));
   }
 
   getMinimalBetAmount() {
--- imports/rulesets/Classic/ClassicRuleset.test.js
+++ imports/rulesets/Classic/ClassicRuleset.test.js
@@ -7,47 +7,236 @@ import ClassicRuleset from "./ClassicRuleset";
 should();
 
 describe('ClassicRuleset', function() {
-  const createActions = () => [
-    {
-      playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'
-    },
-  ];
+  let players = undefined;
+  let actions = [];
+
   const createPlayers = () => [
     {
-      _id: 'Alice'
+      _id: 'Alice',
+      stash: 500
     },
     {
-      _id: 'Bob'
+      _id: 'Bob',
+      stash: 600
     },
     {
-      _id: 'Winston'
+      _id: 'Winston',
+      stash: 600
     },
     {
-      _id: 'Franklin'
+      _id: 'Franklin',
+      stash: 600
     },
     {
-      _id: 'Joseph'
+      _id: 'Joseph',
+      stash: 600
     },
   ];
-  // if no actions ChooseOpponent if one action  initiator should Raise, then Alice should Raise
 
-  it('First pending action should be "ChooseOpponent"', function() {
-    const createRuleset = (actions = [], players = createPlayers(), initiator = 'Joseph') => new ClassicRuleset(actions, players, initiator);
-    const ruleset = createRuleset();
-    const state = ruleset.getState();
-    console.log('state', state);
-    state.pendingActions.should.be.deep.equal([
-      {playerId: 'Joseph', type: 'ChooseOpponent'}
-    ]);
+  this.beforeEach(function () {
+    actions = [];
+    players = createPlayers();
   });
 
-  it('Second pending action should be "Raise"', function() {
-    const createRuleset = (actions = createActions(), players = createPlayers()) => new ClassicRuleset(actions, players);
-    const ruleset = createRuleset();
-    const state = ruleset.getState();
-    state.pendingActions.should.be.deep.equal([
+  const createChooseOpponentAction = (playerId, opponentId) => ({type: 'ChooseOpponent', playerId, opponentId});
+  const createRaiseAction = (playerId, amount) => ({type: 'Raise', playerId, amount});
+  const createStakeAction = (playerId, amount) => ({type: 'Stake', playerId, amount});
+  const createVoteAction = (playerId, candidateId) => ({type: 'Vote', playerId, candidateId});
+
+  it('should provide no messages and a pending action "ChooseOpponent" at the beginning of the game', function() {
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent'}
+    ]);
+    messages.length.should.be.equal(0);
+  });
+
+  it('should provide the message "ChooseOpponent" and a pending action "Raise" after the opponent has been chosen', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
       {playerId: 'Alice', type: 'Raise', amount: 10}
     ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'}
+    ]);
   });
 
+  it('should provide messages ["ChooseOpponent","Raise"] and a pending action "Raise" for Bob after the first Raise has been made with minimal bet set', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Bob', type: 'Raise', amount: 30}
+    ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30}
+    ]);
+  });
+
+  it('should provide messages ["ChooseOpponent","Raise", "Raise"] and a pending action "Raise" for Alice after Bob has been raised the bet', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    actions.push(createRaiseAction('Bob', 50));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'Raise', amount: 50}
+    ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Raise', amount: 50}
+    ]);
+  });
+
+  it('should start Staking for all players after the final bet is set', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    actions.push(createRaiseAction('Bob', 30));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'Stake', amount: 10},
+      {playerId: 'Bob', type: 'Stake', amount: 10},
+      {playerId: 'Winston', type: 'Stake', amount: 10},
+      {playerId: 'Franklin', type: 'Stake', amount: 10},
+      {playerId: 'Joseph', type: 'Stake', amount: 10},
+    ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Raise', amount: 30}
+    ]);
+  });
+
+  it('should provide some "Stake" actions after a few players have made their stakes', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    actions.push(createRaiseAction('Bob', 30));
+    actions.push(createStakeAction('Bob', 50));
+    actions.push(createStakeAction('Winston', 100));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'Stake', amount: 10},
+      {playerId: 'Franklin', type: 'Stake', amount: 10},
+      {playerId: 'Joseph', type: 'Stake', amount: 10},
+    ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Stake', amount: 50},
+      {playerId: 'Winston', type: 'Stake', amount: 100}
+    ]);
+  });
+
+  it('should start Voting for all players after the each player has made his stake', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    actions.push(createRaiseAction('Bob', 30));
+    actions.push(createStakeAction('Bob', 50));
+    actions.push(createStakeAction('Winston', 100));
+    actions.push(createStakeAction('Franklin', 100));
+    actions.push(createStakeAction('Joseph', 10));
+    actions.push(createStakeAction('Alice', 50));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'Vote'},
+      {playerId: 'Bob', type: 'Vote'},
+      {playerId: 'Winston', type: 'Vote'},
+      {playerId: 'Franklin', type: 'Vote'},
+      {playerId: 'Joseph', type: 'Vote'},
+    ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Stake', amount: 50},
+      {playerId: 'Winston', type: 'Stake', amount: 100},
+      {playerId: 'Franklin', type: 'Stake', amount: 100},
+      {playerId: 'Joseph', type: 'Stake', amount: 10},
+      {playerId: 'Alice', type: 'Stake', amount: 50},
+    ]);
+  });
+
+  it('should provide some "Vote" actions after a few players have spent their votes', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    actions.push(createRaiseAction('Bob', 30));
+    actions.push(createStakeAction('Bob', 50));
+    actions.push(createStakeAction('Winston', 100));
+    actions.push(createStakeAction('Franklin', 100));
+    actions.push(createStakeAction('Joseph', 10));
+    actions.push(createStakeAction('Alice', 50));
+    actions.push(createVoteAction('Bob', 'Bob'));
+    actions.push(createVoteAction('Winston', 'Bob'));
+    actions.push(createVoteAction('Franklin', 'Alice'));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'Vote'},
+      {playerId: 'Joseph', type: 'Vote'},
+    ]);
+    messages.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Stake', amount: 50},
+      {playerId: 'Winston', type: 'Stake', amount: 100},
+      {playerId: 'Franklin', type: 'Stake', amount: 100},
+      {playerId: 'Joseph', type: 'Stake', amount: 10},
+      {playerId: 'Alice', type: 'Stake', amount: 50},
+      {playerId: 'Bob', type: 'Vote', candidateId: "Bob"},
+      {playerId: 'Winston', type: 'Vote', candidateId: "Bob"},
+      {playerId: 'Franklin', type: 'Vote', candidateId: "Alice"},
+    ]);
+  });
+
+  it('should provide a pending action "ChooseOpponent" when all votes have been spent', function() {
+    actions.push(createChooseOpponentAction('Alice', 'Bob'));
+    actions.push(createRaiseAction('Alice', 30));
+    actions.push(createRaiseAction('Bob', 30));
+    actions.push(createStakeAction('Bob', 50));
+    actions.push(createStakeAction('Winston', 50));
+    actions.push(createStakeAction('Franklin', 50));
+    actions.push(createStakeAction('Joseph', 50));
+    actions.push(createStakeAction('Alice', 50));
+    actions.push(createVoteAction('Bob', 'Bob'));
+    actions.push(createVoteAction('Winston', 'Bob'));
+    actions.push(createVoteAction('Franklin', 'Alice'));
+    actions.push(createVoteAction('Alice', 'Alice'));
+    actions.push(createVoteAction('Joseph', 'Alice'));
+    const ruleset = new ClassicRuleset(actions, players);
+    const {pendingActions, messages} = ruleset.getState();
+    pendingActions.should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent'}
+    ]);
+
+    // drop composite message here to simplify validation
+    messages.slice(0, 13).should.be.deep.equal([
+      {playerId: 'Alice', type: 'ChooseOpponent', opponentId: 'Bob'},
+      {playerId: 'Alice', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Raise', amount: 30},
+      {playerId: 'Bob', type: 'Stake', amount: 50},
+      {playerId: 'Winston', type: 'Stake', amount: 50},
+      {playerId: 'Franklin', type: 'Stake', amount: 50},
+      {playerId: 'Joseph', type: 'Stake', amount: 50},
+      {playerId: 'Alice', type: 'Stake', amount: 50},
+      {playerId: 'Bob', type: 'Vote', candidateId: "Bob"},
+      {playerId: 'Winston', type: 'Vote', candidateId: "Bob"},
+      {playerId: 'Franklin', type: 'Vote', candidateId: "Alice"},
+      {playerId: 'Alice', type: 'Vote', candidateId: "Alice"},
+      {playerId: 'Joseph', type: 'Vote', candidateId: "Alice"},
+    ]);
+
+    // check if the composite message is provided
+    messages.length.should.be.equal(14);
+  });
 });
