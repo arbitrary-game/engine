7d6c12b263f074e37c87b220680eb81329c56599
Ivan Rozhkov <ivan133133@gmail.com>
2017-02-10 14:25:23 +0300

    default avatars from gravatar

--- imports/common/ShowAvatar.js
+++ imports/common/ShowAvatar.js
@@ -3,14 +3,11 @@ import {Gravatar} from 'meteor/jparker:gravatar'
 export default (user, avatarSize) => {
   if (!avatarSize) avatarSize = 80
   if (user) {
-    if (user.gravatarExistis){
-      return Gravatar.imageUrl( user.md5hash, { secure: true, size: avatarSize, d: 'mm', rating: 'pg' } );
-    } else if (user.profile && user.profile.avatarUrl) {
+    if (user.profile && user.profile.avatarUrl) {
       return user.profile.avatarUrl
     } else {
-      return 'http://api.adorable.io/avatars/' + user._id
+      return Gravatar.imageUrl( user.md5hash, { secure: true, size: avatarSize, d: 'wavatar', rating: 'pg' } );
     }
   }
-
   return "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
 }
