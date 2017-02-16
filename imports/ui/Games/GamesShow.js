e4370eff115f1a604c484982b58edea8103da696
Ivan Rozhkov <ivan133133@gmail.com>
2017-02-16 09:48:48 +0300

    round percent fix

--- imports/ui/Games/GamesShow.js
+++ imports/ui/Games/GamesShow.js
@@ -405,7 +405,7 @@ export class GamesShowComponent extends React.Component {
                   <List.Item icon='like outline' content={<span>Ставка {row.bet}</span>} />
                   <List.Item icon='user' content={row.candidateId === row.playerId ? "на себя" : <span>На кандидата <b>{this.getNameByPlayerId(row.candidateId)}</b> </span>} />
                   <List.Item icon='law' content= { row.winner != null ? ( row.winner ? [<Icon name='trophy'/>, <span>Выигрывает пари {this.getColoredResultNumber(row.prize)}</span>] : <span>Проигрывает пари {this.getColoredResultNumber(row.prize)}</span>) : 'Не участвовал в пари'} />
-                  <List.Item icon='percent' content={<span>Доля в ставке {this.getColoredResultNumber(row.share*100)}%</span>} />
+                  <List.Item icon='percent' content={<span>Доля в ставке {this.getColoredResultNumber(Math.round(row.share*100))}%</span>} />
                   <List.Item icon='cut' content={<span>Скальп {this.getColoredResultNumber(row.scalp)}</span>} />
                   <List.Item icon='circle notched' content={<span>Округление {this.getColoredResultNumber(row.fix)}</span>} />
                   <List.Item icon='line graph' content={<span>Текущий счет {row.total} ({this.getColoredResultNumber(row.total - row.stash)})</span>} />
